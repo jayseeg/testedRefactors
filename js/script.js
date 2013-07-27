557659c2b3bbff27ee82d78ca7c69f8f6624b117
jayseeg <jake_gibbons@the1stmovement.com>
2013-07-27 16:58:02 -0600

    Dropdown email form installed.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 /*empty script file*/
+(function( $ ) {
+
+	if( $('.email-form-li').length ) {
+		var $openTrigger = $('.open');
+
+		$openTrigger.on('click', function( event ) {
+
+			event.preventDefault();
+
+			var $this = $(this);
+
+			$this.next().toggleClass( 'opened' );
+
+		});
+
+	}
+
+})( jQuery );
